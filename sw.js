21f49a6aa53e81d120bea51237c08b5f56173694
stsak19 <zwg9skmvmk@privaterelay.appleid.com>
2026-09-09 22:28:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    σβήνει η παλιά μνήμη και οι πελάτες παίρνουν τα καινούρια αρχεία.
 */
 
-const VERSION = "app-v4";
+const VERSION = "app-v5";
 const SHELL = VERSION + "-shell";
 const RUNTIME = VERSION + "-runtime";
 
